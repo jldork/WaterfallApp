d7404190e0a9ff2d9957220963813a19cc067b1d
James Leung <James.Leung@capitalone.com>
2016-05-18 19:13:55 -0400

    SWITCHED TO REACT

--- client/js/app.js
+++ client/js/app.js
@@ -1,21 +1,10 @@
-/** @jsx m */
+import React from 'react';
+import ReactDOM from 'react-dom';
 
-var Store = require('./store'),
-    m = require('mithril'),
-    Provider = require('mithril-redux').Provider,
-    DatasetUpload = require('./datasetUpload'),
-    EquationInput = require('./equationInput');
+var HelloWorld = React.createClass({
+    render: function() {
+            return (<div>Hello, world!</div>);
+        }
+});
 
-var App = {
-    view: function (ctrl) {
-        return (
-            <div id="waterfall">
-                <h1>Waterfall Chart Generator</h1>
-                <DatasetUpload />
-                <EquationInput />
-            </div>
-        );
-    }
-};
-
-m.mount(document.body, Provider.init(Store, m, App));
+ReactDOM.render(React.createElement(HelloWorld), document.getElementById('root'));
--- client/js/tests/test_app.js
+++
@@ -1,6 +0,0 @@
-var test = require('tape');
-
-test('world is sane', function(t){
-    t.equal(1+1,2);
-    t.end();
-});
